74f2e6403fe2c701b3dcf08cfb9e0f77fff80890
Megan <megan.nyakina@student.moringaschool.com>
2024-08-06 22:15:21 +0300

    adds react route  proggramatic navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./Login";
 
-const router = createBrowserRouter(routes)
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router}/>)
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<div>About page</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
